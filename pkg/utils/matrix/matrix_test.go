8458e8dad08624d849f4202c4111bb13d0dab865
RSYashwanth <yashwanthrs1104@gmail.com>
2024-11-23 03:26:52 -0600

    Added fuzz tests for concurrent multiplication

--- pkg/utils/matrix/matrix_test.go
+++ pkg/utils/matrix/matrix_test.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
 )
 
@@ -58,5 +60,78 @@ func TestAdd(t *testing.T) {
 				t.Errorf("Expected 1, got %f", m3[i][j])
 			}
 		}
+	}
+}
+
+// The abs function is a helper function to get the abs value
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+// The sequentialMultiply function helps fuzz the matrix multiplication test
+func sequentialMultiply(a, b Matrix) (*Matrix, error) {
+	result := make(Matrix, len(a))
+	for i := range result {
+		result[i] = make([]float64, len(b[0]))
+	}
+
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < len(b[0]); j++ {
+			for k := 0; k < len(b); k++ {
+				result[i][j] += a[i][k] * b[k][j]
+			}
+		}
+	}
+
+	return &result, nil
+}
+
+func TestMultiplyFuzz(t *testing.T) {
+	f := func(rowsA, colsA, colsB int, minVal, maxVal float64) bool {
+		rowsA = abs(rowsA % 1000)
+		colsA = abs(colsA % 1000)
+		colsB = abs(colsB % 1000)
+		if rowsA == 0 || colsA == 0 || colsB == 0 {
+			return true
+		}
+
+		// Ensure minVal is less than or equal to maxVal
+		if minVal > maxVal {
+			minVal, maxVal = maxVal, minVal
+		}
+
+		// Generate random matrices
+		matrixA := NewRandomMatrix(rowsA, colsA, minVal, maxVal)
+		matrixB := NewRandomMatrix(colsA, colsB, minVal, maxVal)
+
+		// Perform matrix multiplication with both algorithms
+		correctResult, err1 := sequentialMultiply(*matrixA, *matrixB)
+		if err1 != nil {
+			t.Errorf("Sequential multiply error: %v", err1)
+			return false
+		}
+
+		concurrentResult, err2 := MatrixMultiply(matrixA, matrixB)
+		if err2 != nil {
+			t.Errorf("Concurrent multiply error: %v", err2)
+			return false
+		}
+
+		// Compare the results
+		if !MatrixEquals(correctResult, concurrentResult) {
+			t.Errorf("Results do not match: sequential=%v, concurrent=%v", correctResult, concurrentResult)
+			return false
+		}
+
+		return true
+	}
+
+	it := 100
+	for i := 0; i < it; i++ {
+		fmt.Printf("\rIteration: %d/%d", i+1, it)
+		f(rand.Intn(700), rand.Intn(700), rand.Intn(700), 10.0, 10.0)
 	}
 }
