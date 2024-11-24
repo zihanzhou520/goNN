8b35e105588e336e8cf8427534e7ee519b90d34f
Dhruv Desai <ddesai7@wisc.edu>
2024-11-23 19:31:01 -0600

    Added SGD and Optimizers?

--- pkg/layers/layer.go
+++ pkg/layers/layer.go
@@ -1,11 +1,13 @@
 package layers
 
 import (
+	o "demo/pkg/optimizers/SGD"
 	m "demo/pkg/utils/matrix"
 )
 
 type Layer interface {
 	Forward(input *m.Matrix) (*m.Matrix, error)
-	Backward(input *m.Matrix) (*m.Matrix, error)
+	// Param ??
+	Backward(grad *m.Matrix, optimizer *o.Optimizer) (*m.Matrix, error)
 	Type() string
 }
